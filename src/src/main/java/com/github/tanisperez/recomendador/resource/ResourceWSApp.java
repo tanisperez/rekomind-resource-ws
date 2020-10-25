cc256e304b875e09e9d332f14310d7684d653f99
Estanislao Pérez Nartallo <tanisperez@gmail.com>
2020-10-25 13:28:30 +0100

    Se crea el proyecto Spring Boot con el stack Spring WebFlux.
    - Se añaden los profiles dev, pre y pro.
    - Se configura log4j2 como sistem de logger.

---
+++ src/src/main/java/com/github/tanisperez/recomendador/resource/ResourceWSApp.java
@@ -0,0 +1,12 @@
+package com.github.tanisperez.recomendador.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceWSApp {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(ResourceWSApp.class, args);
+	}
+}
