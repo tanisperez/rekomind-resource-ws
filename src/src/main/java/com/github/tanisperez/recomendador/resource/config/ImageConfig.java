54369c2f2f19822ae3153d973a500717b39f0956
Estanislao Pérez Nartallo <tanisperez@gmail.com>
2020-10-26 20:59:20 +0100

    Se configura el profile de PRE y se añade el Dockerfile.

--- src/src/main/java/com/github/tanisperez/recomendador/resource/config/ImageConfig.java
+++ src/src/main/java/com/github/tanisperez/recomendador/resource/config/ImageConfig.java
@@ -1,5 +1,6 @@
 package com.github.tanisperez.recomendador.resource.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -10,8 +11,14 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 @Configuration
 public class ImageConfig {
 
+	@Value("${resource.routingLocation}")
+	private String routingLocation;
+
+	@Value("${resource.pathLocation}")
+	private String pathLocation;
+
 	@Bean
 	public RouterFunction<ServerResponse> imgRouter() {
-		return RouterFunctions.resources("/images/**", new FileSystemResource("/Users/tanis/Dev/Java/resource-ws/resources/"));
+		return RouterFunctions.resources(this.routingLocation, new FileSystemResource(this.pathLocation));
 	}
 }
