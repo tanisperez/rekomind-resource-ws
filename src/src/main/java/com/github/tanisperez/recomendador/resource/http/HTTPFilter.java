9b1a79c6984cb14654c6ddaae9aa4d7f7342cdc4
Estanislao Pérez Nartallo <tanisperez@gmail.com>
2020-11-01 13:18:23 +0100

    Se añade la IP del cliente que realiza la petición en el log.

--- src/src/main/java/com/github/tanisperez/recomendador/resource/http/HTTPFilter.java
+++ src/src/main/java/com/github/tanisperez/recomendador/resource/http/HTTPFilter.java
@@ -1,7 +1,10 @@
 package com.github.tanisperez.recomendador.resource.http;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.URI;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -17,6 +20,9 @@ public class HTTPFilter implements WebFilter {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HTTPFilter.class);
 
+	/** Unknown host */
+	private static final String UNKNOWN_HOST = "unknown";
+
 	@Override
 	public Mono<Void> filter(final ServerWebExchange exchange, final WebFilterChain chain) {
 		final String request = getRequest(exchange);
@@ -31,9 +37,30 @@ public class HTTPFilter implements WebFilter {
 
 		final StringBuilder request = new StringBuilder();
 		request.append(httpRequest.getMethodValue());
-		request.append(" ");
+		request.append(StringUtils.SPACE);
+		request.append(getHostAddressField(httpRequest));
+		request.append(StringUtils.SPACE);
 		request.append(uri.toString());
 		return request.toString();
+	}
+
+	private static String getHostAddressField(final ServerHttpRequest httpRequest) {
+		final StringBuilder host = new StringBuilder();
+		host.append("(from ");
+		host.append(getHostAddress(httpRequest));
+		host.append(")");
+		return host.toString();
+	}
+
+	private static String getHostAddress(final ServerHttpRequest httpRequest) {
+		final InetSocketAddress remoteAddress = httpRequest.getRemoteAddress();
+		if (remoteAddress != null) {
+			final InetAddress address = remoteAddress.getAddress();
+			if (address != null) {
+				return address.getHostAddress();
+			}
+		}
+		return UNKNOWN_HOST;
 	}
 
 }
