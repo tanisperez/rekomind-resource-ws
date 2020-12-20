3f2a6963e8b3624855b0d159cbc3bb1d342bd965
Estanislao Pérez Nartallo <tanisperez@gmail.com>
2020-12-20 17:05:29 +0100

    Se vincula rekomind-resource-ws con rekomind-common.

--- src/src/main/java/com/rekomind/resource/config/EnvironmentConfig.java
+++ src/src/main/java/com/rekomind/resource/config/EnvironmentConfig.java
@@ -1,7 +1,10 @@
 package com.rekomind.resource.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import com.rekomind.common.audit.property.PropertyLogger;
 
 /**
  * Environment Config using maven profiles.
@@ -12,4 +15,14 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:/${info.app.env.id}/application-${info.app.env.id}.properties")
 public class EnvironmentConfig {
 
+	/**
+	 * Create a {@code PropertyLogger} bean to print by console every property
+	 * configured.
+	 *
+	 * @return New instance of {@code PropertyLogger}.
+	 */
+	@Bean
+	public PropertyLogger propertyLogger() {
+		return new PropertyLogger();
+	}
 }
